1c0e0267f89c0e0c884115d1e3aef10931368d6a
nils <nils@aleksale.lv>
2024-02-19 16:13:07 +0200

    news done

--- home.php
+++ home.php
@@ -7,10 +7,16 @@ $dbname = "prakse";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "SELECT news_id, name,  main_image, context FROM news";
-$results = $conn->query($sql);
+
+
+$newsSql = "SELECT news_id, name,  main_image, context FROM news ORDER BY news_id DESC";
+
+
+$newsResults = $conn->query($newsSql);
+
 
 $news_id = $_GET['news_id'];
+
 
 ?>
 
@@ -18,22 +24,45 @@ $news_id = $_GET['news_id'];
     <div class="col-md-8">
         <section class="row">
             <article>
-                <h1>Heading 1</h1>
-                    <div class="d-flex flex-wrap">
-                        <div class="col-md-6">
-                            <img src="https://picsum.photos/200/300">
-                        </div> 
+                <!-- <h1>Heading 1</h1>
+                <div class="d-flex flex-wrap">
+                    <div class="col-md-6">
+                        <img src="https://picsum.photos/200/300">
+                    </div> 
 
-                        <div class="col-md-6">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida metus semper leo imperdiet, at maximus sem blandit. Donec varius neque sem, vel elementum lorem condimentum eget. Aenean id nisi non elit rhoncus tincidunt. Suspendisse ac euismod mi. Sed consectetur turpis lectus, a lobortis enim ultrices quis. Proin tempor auctor venenatis. Aenean eu neque pellentesque, laoreet arcu eget, imperdiet ante.</p>
-                        </div> 
-                    </div>
+                    <div class="col-md-6">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida metus semper leo imperdiet, at maximus sem blandit. Donec varius neque sem, vel elementum lorem condimentum eget. Aenean id nisi non elit rhoncus tincidunt. Suspendisse ac euismod mi. Sed consectetur turpis lectus, a lobortis enim ultrices quis. Proin tempor auctor venenatis. Aenean eu neque pellentesque, laoreet arcu eget, imperdiet ante.</p>
+                    </div> 
+                </div> -->
+
+                <?php 
+                    $count = 0;
+
+                    foreach ($newsResults as $result){
+
+                        if ($count == 0){
+                        ?>
+                        <h1> <?php echo $result['name']; ?></h1>
+                        <div class="d-flex flex-wrap p-2">
+                            <div class="col-md-6">
+                                <img class="rounded border border-secondary w-75" src="<?php echo $result['main_image']; ?>">
+                            </div>
+                    
+                            <div class="col-md-6 ">
+                                <p><?php echo $result['context']; ?></p>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                    $count++;
+                    }
+                ?>
             </article>
         </section>
 
         <section class="row">
             <article>
-                <h2>Heading 2</h2>
+                <!-- <h2>Heading 2</h2>
                 <div class="d-flex flex-wrap">
                     <div class="col-md-6">
                         <p>Sed pulvinar felis et pulvinar auctor. Suspendisse a pellentesque nibh. Phasellus mattis facilisis elit, eu eleifend lorem maximus id. Sed nec commodo nisi, quis mollis lorem. Proin porta erat a dui scelerisque mollis. In hac habitasse platea dictumst. Morbi nec massa eget erat dapibus mattis.</p>
@@ -44,7 +73,52 @@ $news_id = $_GET['news_id'];
                     <div class="">
                         <img src="https://picsum.photos/400/100">
                     </div>
-                </div>
+                </div> -->
+                <?php 
+                    $count = 0;
+
+                    foreach ($newsResults as $result){
+
+                        if ($count == 1){
+                        ?>
+                        <h2><?php echo $result['name']; ?></h2>
+                        <div class="d-flex flex-wrap">
+                            <?php
+                                $text_length = strlen($result['context']);
+                                $half_text = $text_length / 2;
+
+                                $col_limit = 200;
+
+                                $class = 12;
+                                if ($half_text >=  $col_limit){
+                                    ?>
+                                        <div class="col-md-6 ">
+                                        <p><?php echo substr($result['context'], $half_text); ?></p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p><?php echo substr($result['context'], -$half_text); ?></p>
+                                        </div>
+                                    <?php
+                                }else{
+                                    ?>
+                                        <div class="col-md-12 ">
+                                        <p><?php echo $result['context']; ?></p>
+                                        </div>
+                                    <?php
+                                }
+                                
+                                
+                                ?>
+                            <div class="col-md-6 h-25">
+                                <img class="rounded border border-secondary img-fluid w-100" src="<?php echo $result['main_image']; ?>">                              
+                            </div>
+                        </div>
+                            
+                        <?php
+                        }
+                    $count++;
+                    }
+                ?>
             </article>
         </section>
     </div>
@@ -53,63 +127,30 @@ $news_id = $_GET['news_id'];
         <aside>
             <div class="container bg-dark-subtle">
                 <div class="row">
-                    <!-- <ul class="list-unstyled">
-                        <li>
-
-                            <div class="d-flex align-items-center m-2">
-                                <div class="col-4">
-                                    <img class="img-thumbnail" src="https://picsum.photos/100">
-                                </div>
-                                <div class="ms-3 col-8">
-                                    <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
-                                </div>
-                                </div>
-                            
-                        </li>
-                        
-                        <li>
-                            <div class="d-flex align-items-center m-2">
-                                <div class="col-4">
-                                    <img class="img-thumbnail" src="https://picsum.photos/100">
-                                </div>
-
-                                <div class="ms-3 col-8">
-                                    <p>Sed sollicitudin augue sed ex fermentum efficitur Lorem ipsum dolor sit amet</p>
-                                </div>
-                            </div>
-                        </li>
-
-                        <li>
-                            <div class="d-flex align-items-center m-2">
-                                <div class="col-4">
-                                    <img class="img-thumbnail" src="https://picsum.photos/100">
-                                </div>
-
-                                <div class="ms-3 col-8">
-                                    <p>Ut eu eros sed enim sodales laoreet Lorem ipsum dolor sit amet</p>
-                                </div>
-                            </div>
-                        </li>     
-                    </ul> -->
-
                     <?php 
-                        foreach ($results as $result){
-                    ?>
-                    <div class="d-flex align-items-center m-2">
-                        <div class="col-md-4">
-                            <a href="?news_id&news_id=<?php echo $result["news_id"]; ?>">
-                                <img class="rounded border border-secondary w-75" src="<?php echo $result['main_image']; ?>">
-                                
-                                    <p><?php echo $result['name']; ?></p>
-                                
-                            </a>
-                        </div>
-                
-                        <div class="p-3 col-md-8 ">
-                            <p><?php echo $result['context']; ?></p>
-                        </div>
-                    </div>
-                    <?php
+                        $count = 0;
+
+                        foreach ($newsResults as $result){
+
+                            if ($count >= 2){
+                            ?>
+                            <div class="d-flex align-items-center m-2">
+                                <div class="col-md-4">
+                                    <a href="?news_id&news_id=<?php echo $result["news_id"]; ?>">
+                                        <img class="rounded border border-secondary w-75" src="<?php echo $result['main_image']; ?>">
+                                        
+                                            <p><?php echo $result['name']; ?></p>
+                                        
+                                    </a>
+                                </div>
+                        
+                                <div class="p-3 col-md-8 ">
+                                    <p><?php echo $result['context']; ?></p>
+                                </div>
+                            </div>
+                            <?php
+                         }
+                        $count++;
                         }
                     ?>
                 </div>       
--- news_id.php
+++ news_id.php
@@ -15,8 +15,36 @@ $name = mysqli_fetch_array($results);
 
 
 
-var_dump($news_id['news_id']);
+// var_dump($news_id['news_id']);
 
 echo "<h1>". $name['name'] ."</h1>"
 
+
+
 ?>
+
+<div class="content row">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="d-flex flex-wrap text-center mx-2">
+            <?php 
+                foreach ($results as $result){
+            ?>
+                <div class="col-md-6 p-3">
+                        <img class="rounded border border-secondary" src="<?php echo $result['main_image']; ?>">
+                        <div class="p-2 w-100">
+                            
+                            <p><?php echo $result['name']; ?></p>
+                        </div>
+                </div> 
+                <div class="col-md-6 p-3">
+                    <p><?php echo $result['context']; ?></p> 
+                </div> 
+            <?php
+                }
+            ?>
+
+            </div>
+        </div>
+    </div>
+</div>
